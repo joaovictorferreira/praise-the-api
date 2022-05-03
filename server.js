07e6bf5790a79b4cceb2f32a1df791c0fd34df48
Joao <joaovictorfrra@gmail.com>
2022-05-03 20:07:55 -0300

    swagger

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const routes = require('./routes/route');
+const swaggerUi = require('swagger-ui-express');
+const sjson = require("./doc/swagger.json");
 const app = express();
 const port = process.env.PORT || 3000
 
+
 app.use(express.json());
 app.use('/bosses', routes)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(sjson))
 
 app.listen(port, () =>{
     console.log(`listening on port ${port}`)
