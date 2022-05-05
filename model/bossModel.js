f67fdda96badf39905bad98885bdf32e70379ad7
Joao <joaovictorfrra@gmail.com>
2022-05-04 21:37:22 -0300

    changes in the getBossByName method

--- model/bossModel.js
+++ model/bossModel.js
@@ -23,7 +23,7 @@ class bossModel{
                 if(error) {return res.status(500).send({error: error})}
                 if(result.length == 0) {return res.status(404).send("boss not found")}
                 
-                if(bossId != "normal-ornstein" && "normal-smough") {
+                if(bossId != "normal-ornstein" && bossId != "normal-smough") {
                   return res.status(200).send({boss: result, drops:`https://praise-the-api.herokuapp.com/bosses/drops/${bossId}`})
                 } else {
                   return res.status(200).send({boss: result})
